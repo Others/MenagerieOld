b7537bbbc34f2ae72e0eae65945abc9665471c46
Gregor Peach <gregorpeach@gmail.com>
2015-09-15 17:11:54 -0700

    Change Individual, and Population Metaheuristic into interfaces
    This commit makes both the abstract metaheuristics more interfacey.
    It also adds some convinece methods to individual metaheuristic.

--- src/menagerie/metaheuristic/IndividualMetaheuristic.java
+++ src/menagerie/metaheuristic/IndividualMetaheuristic.java
@@ -2,21 +2,22 @@ package menagerie.metaheuristic;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import menagerie.genome.Genome;
 import menagerie.genome.GenomeSpec;
 
-public abstract class IndividualMetaheuristic extends PopulationMetaheuristic{
-	public abstract Genome optimizeIndividual(GenomeSpec spec, Genome genome, Instant endTime);
+public interface IndividualMetaheuristic{
+	public Genome optimizeIndividual(GenomeSpec spec, Genome genome, Instant endTime);
 	
-	public final Genome optimizeIndividual(GenomeSpec spec, Genome genome, Duration timeBudget){
+	public default Genome optimizeIndividual(GenomeSpec spec, Genome genome, Duration timeBudget){
 		return optimizeIndividual(spec, genome, Instant.now().plus(timeBudget));
 	}
 	
-	public Set<Genome> optimizePopulation(GenomeSpec spec, Set<Genome> pool, Instant endTime){
-		Duration perGenome = Duration.between(Instant.now(), endTime).dividedBy(pool.size());
-		return pool.parallelStream().map(g -> this.optimizeIndividual(spec, g, perGenome)).collect(Collectors.toSet());
-	} 
+	public default Genome optimizeIndividual(GenomeSpec spec, Instant endTime){
+		return optimizeIndividual(spec, spec.makeRandomGenome(), endTime);
+	}
+	
+	public default Genome optimizeIndividual(GenomeSpec spec, Duration timeBudget){
+		return optimizeIndividual(spec, Instant.now().plus(timeBudget));
+	}
 }
--- src/menagerie/metaheuristic/PopulationMetaheuristic.java
+++ src/menagerie/metaheuristic/PopulationMetaheuristic.java
@@ -8,23 +8,22 @@ import java.util.Set;
 import menagerie.genome.Genome;
 import menagerie.genome.GenomeSpec;
 
-public abstract class PopulationMetaheuristic {
+public interface PopulationMetaheuristic {
+	public Set<Genome> optimizePopulation(GenomeSpec spec, Set<Genome> pool, Instant endTime);
 	
-	public abstract Set<Genome> optimizePopulation(GenomeSpec spec, Set<Genome> pool, Instant endTime);
-	
-	public final Set<Genome> optimizePopulation(GenomeSpec spec, Set<Genome> pool, Duration timeBudget){
+	public default Set<Genome> optimizePopulation(GenomeSpec spec, Set<Genome> pool, Duration timeBudget){
 		return optimizePopulation(spec, pool, Instant.now().plus(timeBudget));
 	}
 	
-	public final Set<Genome> optimizePopulation(GenomeSpec spec, int poolSize, Duration timeBudget){
+	public default Set<Genome> optimizePopulation(GenomeSpec spec, int poolSize, Duration timeBudget){
 		return optimizePopulation(spec, getGenomes(spec, poolSize), timeBudget);
 	}
 	
-	public final Set<Genome> optimizePopulation(GenomeSpec spec, int poolSize, Instant endTime){
+	public default Set<Genome> optimizePopulation(GenomeSpec spec, int poolSize, Instant endTime){
 		return optimizePopulation(spec, getGenomes(spec, poolSize), endTime);
 	}
 	
-	public Set<Genome> getGenomes(GenomeSpec spec, int count){
+	public default Set<Genome> getGenomes(GenomeSpec spec, int count){
 		Set<Genome> genomes = new HashSet<>(count);
 		for(int i=0; i < count; i++){
 			genomes.add(spec.makeRandomGenome());
